ac0070c0ac4e7a6e7a2ad773795c5e633ac9dd1f
3rdIteration <steve@cryptoguide.tips>
2020-10-14 16:39:53 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # jplink-location-maps-to-kml
  A simple script to filter & convert JPList based location maps (eg: for stores) in to KML that you can use with other mapping tools.
+ 
+ It currently parses all data tags, icons, etc, but is only using the name and location in terms of output to KML. (Though it is trivial to add additional data to the KML )
+ 
+ ## Usage
+ Straight Conversion
+ 
+` python convert.py --inputfile "JPList Page.html" --includeClassData "Presbytery: Halifax Lunenburg" --outputfile "Output File.kml"
+`
+
+ Only Including items with a specific data tag
+ 
+`python convert.py --inputfile "JPList Page.html" --includeClassData "Presbytery: Halifax Lunenburg" --outputfile "Output File.kml"
+` 
+ ## Requirements:
+ SimpleKML library. (Easily installed via PIP)
---
+++ convert.py
@@ -0,0 +1,146 @@
+# convert.py
+# 2020 - Stephen Rothery
+
+import sys, argparse, simplekml, html
+
+__version__ =  "0.1"
+
+if __name__ == "__main__":
+    print("Starting JPLink Location Data Conversion", __version__)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--inputfile", help="The file that contains a the main HTML file containing the locator map data")
+    parser.add_argument("--outputfile", help="The file that will contain the KML data")
+    parser.add_argument("--includeClassData", help="The file that contains a the main HTML file containing the locator map data")
+
+    args = parser.parse_args()
+
+    if not args.inputfile:
+        sys.exit("Please specify an input file...")
+
+    try:
+        kmlfile = simplekml.Kml()
+        with open(args.inputfile) as jplink_data:
+
+            # A few variables to keep track of the file as we parse it
+            storeData = False
+            lastLine = ""
+            skipLines = 0
+
+            # Blank varibles for a location
+            pointLocation = []
+            icon = ""
+            data = []
+
+            # File Parser
+            for line in jplink_data:
+                # Clean up the input a bit
+                line = line.strip()
+                line = html.unescape(line)
+
+                # Skip blank lines
+                if len(line) == 0:
+                    continue
+
+                # Skip blocks of lines
+                if skipLines > 0:
+                    skipLines -= 1
+                    continue
+
+                # What do do when we arrive at the start of a new location
+                if "<!-- store -->" in line:
+                    storeData = True
+
+                    # Check to see if this is the very first location
+                    if len(data) > 0:
+                        include = True
+
+                        # Iterate through the data tags and record any that we want to include in the KML file
+                        # MODIFY HERE TO ADD YOUR OWN DATA TAGS TO KML
+                        for itemName, itemValue in data:
+                            if 'title' in itemName:
+                                pointName = itemValue
+
+                        # Check to see if we are filtering based on the data tag values
+                        if args.includeClassData:
+                            include = False
+                            for itemName, itemValue in data:
+                                if args.includeClassData in itemValue:
+                                    include = True
+
+                        # Include the location in the KML file if required
+                        if include:
+                                # MODIFY HERE TO ADD YOUR OWN DATA TAGS TO KML
+                                kmlfile.newpoint(name = pointName, coords = [(pointLocation_long, pointLocation_lat)])
+
+                    # Clear the location data, ready fo the next location
+                    pointName = ""
+                    pointLocation = []
+                    icon = ""
+                    data = []
+                    continue
+
+                # Parse through the data
+                if "</div>" in line and "</div>" in lastLine:
+                    storeData = False
+                    continue
+                lastLine = line
+
+                if "<!-- directions -->" in line:
+                    skipLines = 7
+                    continue
+
+                if "<div" in line or "</div" in line:
+                    continue
+
+                # Processing Lines
+                if storeData:
+
+                    if "data-latitude" in line:
+                        data_latitude = line.split("=")
+                        pointLocation_lat = float(data_latitude[1].strip('"'))
+                        continue
+
+                    if "data-longitude=" in line:
+                        data_longitude = line.split("=")
+                        pointLocation_long = float(data_longitude[1].strip('">'))
+                        pointLocation = tuple(pointLocation)
+                        continue
+
+                    #Items that can work with one split.
+                    splitLine = line.split(" ",1)
+
+                    try:
+                        if "data-marker-icon" in splitLine[1]:
+                            icon = splitLine[1].split("=")[1]
+                            continue
+
+                        if "data-type=" in splitLine[1]:
+                            if "<p hidden>" in splitLine[1]:
+                                splitLine = splitLine[1].split('"><p hidden>', 1)
+                                dataName = splitLine[0].split('"')[1]
+                                dataValue = splitLine[1].split("<")[0]
+                                data.append((dataName, dataValue))
+                                continue
+
+                            splitLine = splitLine[1].split('">', 1)
+                            dataName = splitLine[0].split('"')[1]
+                            dataValue = splitLine[1].split("<")[0]
+                            data.append((dataName, dataValue))
+                            continue
+
+                        # Items that can work with two splits.
+                        splitLine = splitLine[1].split(" ",1)
+                        dataName = splitLine[0].split("=")[1].strip('"')
+                        dataValue = splitLine[1].split(">")[1].split('<')[0]
+                        data.append((dataName,dataValue))
+                        continue
+
+                    except IndexError:
+                        pass
+                        print("Ignoring Line:", line)
+
+        # Output the KML once everything is done
+        kmlfile.save(args.outputfile)
+
+    except IOError:
+        print("File:", args.inputfile, "Not Found")
